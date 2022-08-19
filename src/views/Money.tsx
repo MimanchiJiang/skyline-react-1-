8134791165e75051198b49442a66bcbc85045b66
JHuaWei <2237268069@qq.com>
2022-08-19 17:16:35 +0800

    完成备注的功能

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,10 +1,10 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import styled from 'styled-components';
-import {TagsSection} from './Money/TagsSection';
-import {CategorySection} from './Money/CategorySection';
-import {NotesSection} from './Money/NotesSection';
-import {NumberPadSection} from './Money/NumberPadSection';
+import { TagsSection } from './Money/TagsSection';
+import { CategorySection } from './Money/CategorySection';
+import { NoteSection } from './Money/NoteSection';
+import { NumberPadSection } from './Money/NumberPadSection';
 
 const MyLayout = styled(Layout)`
   display:flex;
@@ -13,21 +13,8 @@ const MyLayout = styled(Layout)`
 function Money() {
   return (
     <MyLayout>
-      <TagsSection>
-        <ol>
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
-        </ol>
-        <button>新增标签</button>
-      </TagsSection>
-      <NotesSection>
-        <label>
-          <span>备注</span>
-          <input type="text" placeholder="在这里添加备注"/>
-        </label>
-      </NotesSection>
+      <TagsSection />
+      <NoteSection />
       <CategorySection>
         <ul>
           <li className="selected">支出</li>
