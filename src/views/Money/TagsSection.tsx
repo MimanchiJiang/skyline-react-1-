cadb5b372f672534ac9994c3c65b4bb4e3c362dc
JHuaWei <2237268069@qq.com>
2022-08-20 16:55:46 +0800

    抽离useState

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components';
+import useTags from 'useTags';
 
 const Wrapper = styled.section`
   background: #FFFFFF; padding: 12px 16px;
@@ -24,7 +25,8 @@ const Wrapper = styled.section`
 type Props = { value: string[]; onChange: (selected: string[]) => void }
 
 const TagsSection: React.FC<Props> = (props) => {
-  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行'])
+  const { tags, setTags } = useTags()
+
   const selectedTags = props.value;
   const onAddTag = () => {
     const tagName = window.prompt('新标签的名称为')
