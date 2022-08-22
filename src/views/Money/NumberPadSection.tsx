43b924839965b9e6ece245c2ac32c10970f773ba
JHuaWei <2237268069@qq.com>
2022-08-22 16:52:49 +0800

    修复无法输入小数的bug

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -11,17 +11,19 @@ type Props = {
 }
 
 const NumberPadSection: React.FC<Props> = (props) => {
-  const output = props.value.toString()
+  // const output = props.value.toString()
+  const [output, _setOutput] = useState(props.value.toString())
   const setOutput = (output: string) => {
-    let value
+    let newOutput: string
     if (output.length > 16) {
-      value = parseFloat(output.slice(0, 16))
+      newOutput = output.slice(0, 16)
     } else if (output.length === 0) {
-      value = 0
+      newOutput = '0'
     } else {
-      value = parseFloat(output)
+      newOutput = output
     }
-    props.onChange(value);
+    _setOutput(newOutput)
+    props.onChange(parseFloat(newOutput));
   }
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent
@@ -33,7 +35,7 @@ const NumberPadSection: React.FC<Props> = (props) => {
       }
       return
     }
-    if ('0123456789'.split('').concat(['删除', '清空']).indexOf(text) >= 0) {
+    if ('0123456789.'.split('').concat(['删除', '清空']).indexOf(text) >= 0) {
       setOutput(generateOutput(text, output))
 
     }
